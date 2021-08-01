6503379fcbe946d3fa17d5f26899716d05c90467
username <euny_sky11@naver.com>
2021-08-01 13:09:54 +0900

    21.08.01 project fin :)

--- mouseEffect.js
+++ mouseEffect.js
@@ -1,6 +1,7 @@
 let mouseCursor = document.querySelector(".cursor");
-let navLinks = document.querySelectorAll(".menu-flexbox a"); //메뉴 링크
-let img = document.querySelectorAll(".like-contents img");
+let navLinks = document.querySelectorAll(".menu-flexbox a");
+let navLinks2 = document.querySelectorAll(".email");//메뉴 링크
+let contactLink = document.querySelectorAll(".contact-contents span");
 //window 객체에 scroll & mouse 이벤트를 추가하고 cursor함수 실행되도록 함
 
 window.addEventListener("scroll", cursor);
@@ -12,16 +13,49 @@ function cursor(e) {
   mouseCursor.style.top = e.pageY - scrollY + "px";
   }
 
+
   navLinks.forEach((link) => {
     link.addEventListener("mouseover", () => {
-      mouseCursor.classList.add("cursor-grow");
+      mouseCursor.classList.add("cursor-white");
       mouseCursor.style.zIndex = "-1";  
-      link.classList.add("hovered-link");
+      link.classList.add("hovered-black");
     });
     
     link.addEventListener("mouseleave", () => {
-      mouseCursor.classList.remove("cursor-grow");
+      mouseCursor.classList.remove("cursor-white");
       mouseCursor.style.zIndex = "1000";
-      link.classList.remove("hovered-link");
+      link.classList.remove("hovered-black");
+    });
+  });
+
+
+  // contact-email hover effect
+  navLinks2.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      mouseCursor.classList.add("cursor-black");
+      mouseCursor.style.zIndex = "-1";  
+      link.classList.add("hovered-white");
+    });
+    
+    link.addEventListener("mouseleave", () => {
+      mouseCursor.classList.remove("cursor-black");
+      mouseCursor.style.zIndex = "1000";
+      link.classList.remove("hovered-white");
+    });
+  });
+
+
+
+  contactLink.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      mouseCursor.classList.add("cursor-white");
+      mouseCursor.style.zIndex = "-1";  
+      link.classList.add("hovered-white");
+    });
+    
+    link.addEventListener("mouseleave", () => {
+      mouseCursor.classList.remove("cursor-white");
+      mouseCursor.style.zIndex = "1000";
+      link.classList.remove("hovered-white");
     });
   });
